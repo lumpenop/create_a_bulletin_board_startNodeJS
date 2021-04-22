be03776e3901b353e8ef22a408997fff9de2cacc
lumpenop <swk5373@gmail.com>
2021-04-22 16:34:47 +0900

    add .env, main.html

--- server.js
+++ server.js
@@ -17,12 +17,11 @@ let connection = mysql.createConnection({ // mysql을 연결하여 database를 �
     host:'localhost',
     user:'root',
     password:'1234',
-    database:'node1',
+    database:'nodejs_1',
 });
 
 connection.connect();
-app.get('/', (req, res)=>{
-                                            // results는 배열 내에 객체가 있는 형태
+app.get('/', (req, res)=>{    // results는 배열 내에 객체가 있는 형태
     connection.query("select * from user", (error, results)=>{
         if(error){
             console.log('error');
@@ -35,7 +34,7 @@ app.get('/', (req, res)=>{
     })
     
     res.render('index.html' ,{
-        title:'na',
+        title:'sw',
     })
 });
 
@@ -43,23 +42,30 @@ app.get('/', (req, res)=>{
 app.get('/join', (req, res)=>{
                                         
     res.render('join.html' ,{
-      
     })
-
 });
 
 app.get('/login', (req, res)=>{
-                                        
+
     res.render('login.html' ,{
-        
     })
 });
 
-app.post('/main', (req, res)=>{
+app.get('/main', (req, res)=>{
                                         
-    res.render('main.html' ,{
-        id:req.body.userid,
+    connection.query('select * from board', (error, results)=>{
+        if(error){
+            console.log('error');
+        } else{
+            
+            console.log('it works');
+            results.forEach((ele)=>{
+                res.send([ele.idx, ele.subject, ele.content, ele.date, ele.hit]);
+            })
+        }
     })
+
+    
 });
 
 
@@ -77,7 +83,7 @@ app.post('/join', (req, res)=>{
            
             res.render('login.html', {
                 id:req.body.userid,
-                name: req.body.userbody,
+                name: req.body.username,
                 password : req.body.userpw,
             })
         }
@@ -99,7 +105,7 @@ app.listen(port, ()=>{
 2.limit 기능 - limit 뒤의 수가 1개일 때는 1번 부터 숫자 만큼, 2개일 때는 a번 부터 ~ b 번째 까지 조회
 3.type조사 - int(n): 숫자(n byte) , varchar(n):가변 문자열(n byte), text:긴 문자열, enum:해당 집합 내의 데이터만 받는 데이터 타입,
 4.not null - null이 없어야하는 제약조건
-5.%기능 - % % 사이의 문자에 해당하는 데이터 조회
+5.%기능 - '% %' 사이의 문자에 해당하는 데이터 조회, '%a' a로 시작하는, 'a%' 제목으로 끝나는
 6.primary key - 기본 키 제약조건으로 not null과 unique key 제약 조건을 포함, 테이블 내에서 유일한 식별자로 사용, 다른 테이블에서 foreign key로 사용하여 참조 가능
 
 */
